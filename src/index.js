10a1d7cb3ba434f6365372ef506fa7148364be09
slarribeau <slarribeau@gmail.com>
2018-11-25 10:16:16 -0800

    add select logic

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+    foo(e) {
+        console.log(e.target.value)
+    }
 
     render() {
         console.log("render")
         return(
             <div >
-              Hello!
+                Hello!
+                <select onChange={this.foo}>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+
+                <select onChange={ (e) => console.log(e.target.value) }>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
             </div>
         );
     }
